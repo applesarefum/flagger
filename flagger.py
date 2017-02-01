9ab83d39894ef5670a3650c9babb787d853d3600
William Muhlbach <applesarefum>
2017-02-01 08:27:06 -0500

    save

--- flagger.py
+++ flagger.py
@@ -1,9 +1,11 @@
 #Eventually...
-#user will be prompted if a required variable is not defined
-
+#if a required variable is not defined, user will be prompted
+#if input provided doesn't match proper type, user will be prompted
 import sys
 
 def action(flag,function):
     if flag in sys.argv:
         function()
-
+def variable(flag):
+    if flag in sys.argv:
+        
