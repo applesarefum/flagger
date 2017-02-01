9f3748d3ec7a10f53ed7e56d1c7963b510e1d376
William Muhlbach <applesarefum>
2017-02-01 12:46:27 -0500

    save

--- flagger.py
+++ flagger.py
@@ -3,15 +3,22 @@
 #if input provided doesn't match proper type, user will be prompted
 import sys
 
-def action(flag,function):
+def action(flag,function,exit=False):
     for thing in sys.argv:
         if thing.startswith("-"):
             if flag in thing:
                 function()
+#idk if this is necessary
+#    if exit==True:
+#        exit()
+#    elif exit==False:
+#        pass
+#    else:
+#        bool(exit)
 def action2(flag,function):
     for thing in sys.argv:
         if flag in thing:
             function()
-def variable(flag,vartype,required):
+def variable(flag):
     if flag in thing:
         pass
