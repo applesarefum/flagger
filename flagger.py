8da77cc317b47032c72a855be4a0f4f868168507
William Muhlbach <applesarefum>
2017-02-01 22:34:09 -0500

    more or less functional (jk it's totally more)

--- flagger.py
+++ flagger.py
@@ -6,17 +6,15 @@ import sys
 
 def action(flag,function):
     for arg in sys.argv:
-        if arg.startswith("-"):#This requires the user to preface their flag with a "-"
-            if flag in arg:
-                function()
-#                if exit==True:
-#                    exit()
-#                elif exit==False:
-#                    pass
-#                else:
-#                    bool(exit)
+        if arg.startswith("-") and flag in arg:
+            function()
+#            if exit==True:
+#                exit()
+#            elif exit==False:
+#                pass
+#            else:
+#                bool(exit)
 def getval(flag):
-    for arg in sys.argv:
-        if arg.startswith("-"):#This requires the user to preface their flag with a "-"
-            if flag in arg:
-                return sys.argv.next()
+    for arg in (sys.argv):
+        if arg.startswith("-") and flag in arg:
+            return sys.argv.index(arg)
