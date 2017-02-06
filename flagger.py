e754623eb62d4a035d7f130b36401dbcb3fe6c73
William Muhlbach <applesarefum>
2017-02-06 15:29:29 -0500

    pogram

--- flagger.py
+++ flagger.py
@@ -1,20 +1,21 @@
 #Eventually...
 #if a required variable is not defined, user will be prompted
 #if input provided doesn't match proper type, user will be prompted
-#option to have pogram terminate after an action
+#option to have program terminate after an action
+#remove arguments from sys.argv after they've been used
 import sys
 
-def action(flag,function):
+def action(flag,function,exit=False):
     for arg in sys.argv:
         if arg.startswith("-") and flag in arg:
             function()
-#            if exit==True:
-#                exit()
-#            elif exit==False:
-#                pass
-#            else:
-#                bool(exit)
-def getval(flag):
+            if exit==True:
+                exit()
+            elif exit==False:
+                pass
+            else:
+                bool(exit)
+def getval(flag,type=string):
     for arg in (sys.argv):
         if arg.startswith("-") and flag in arg:
             return sys.argv[sys.argv.index(arg)+1]
