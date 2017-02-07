e714ac2c77c2e40f5db6346814005399dcd61984
William Muhlbach <applesarefum>
2017-02-07 09:25:45 -0500

    save

--- flagger.py
+++ flagger.py
@@ -1,19 +1,26 @@
 #Eventually...
 #if a required variable is not defined, user will be prompted
 #if input provided doesn't match proper type, user will be prompted
-#option to have program terminate after an action
-#remove arguments from sys.argv(is that a possible?) after they've been used
+#remove arguments from sys.argv (is that a possible?) after they've been used
+#quit if an unrecognized flag is provided
+
 import sys
 
-def action(flag,function,stop=False):
+def action(flag,function,quit=False):
     for arg in sys.argv:
         if arg.startswith("-") and flag in arg:
             function()
-            if stop:
+            if quit:
                 exit()
+
+def present(flag):
+    for arg in sys.argv:
+        if arg.startswith("-") and flag in arg:
+            return True
+
 def getval(flag,type='string'):
     for arg in (sys.argv):
         if arg.startswith("-") and flag in arg:
             return sys.argv[sys.argv.index(arg)+1]
-#FOR THE LOVE OF GOD THIS IS SO DISGUSTING OH MY GOD
-#PLEASE LEARN HOW TO USE ENUMERATE()
+            #FOR THE LOVE OF GOD THIS IS SO DISGUSTING OH MY GOD
+            #PLEASE LEARN HOW TO USE ENUMERATE()
