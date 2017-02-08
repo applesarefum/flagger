9429ce8cd74d3e22d8e85feb0c2d270faa7a4f36
William Muhlbach <applesarefum>
2017-02-08 12:01:05 -0500

    I made it better, but worse

--- flagger.py
+++ flagger.py
@@ -3,8 +3,14 @@
 #if input provided doesn't match proper type, user will be prompted
 #remove arguments from sys.argv (is that a possible?) after they've been used
 #quit if an unrecognized flag is provided
-
+#add failsafe if a flag triggers getval but no input it provided
+"""GETVAL IS BROKEN"""
 import sys
+
+def present(flag):
+    for arg in sys.argv:
+        if arg.startswith("-") and flag in arg:
+            return True
 
 def action(flag,function,quit=False):
     if present(flag):
@@ -12,13 +18,8 @@ def action(flag,function,quit=False):
         if quit:
             exit()
 
-def present(flag):
-    for arg in sys.argv:
-        if arg.startswith("-") and flag in arg:
-            return True
-
-def getval(flag,type='string'):
-    if present(flag):
-        return sys.argv[sys.argv.index(arg)+1]
+#def getval(flag,type='string'):
+#    if present(flag):
+#        return sys.argv[sys.argv.index(arg)+1]
         #FOR THE LOVE OF GOD THIS IS SO DISGUSTING OH MY GOD
         #PLEASE LEARN HOW TO USE ENUMERATE()
